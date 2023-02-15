d3b75cf7787b8a568de62d6395ffd29b975b823b
Hammed Oyedele <itz.harmid@gmail.com>
2023-02-15 12:25:22 +0100

    refactor: improve the model directive

--- lib/src/directives/model.ts
+++ lib/src/directives/model.ts
@@ -4,31 +4,67 @@ directive('*model', () => {
   return {
     newContext: false,
     apply(el, context, exp: string) {
-      const isCheckbox =
-        el instanceof HTMLInputElement && el.type === 'checkbox';
+      const isCheckboxOrRadio =
+        el instanceof HTMLInputElement &&
+        ['checkbox', 'radio'].indexOf(el.type) > -1;
       const eventName =
-        el instanceof HTMLSelectElement || isCheckbox ? 'change' : 'input';
+        el instanceof HTMLSelectElement || isCheckboxOrRadio
+          ? 'change'
+          : 'input';
       const setModelValue = (value: any) => {
         context.$set(exp, value);
         context.$notify();
       };
       const getModelValue = () => context.$eval(exp);
-      const getElValue = () => (isCheckbox ? el.checked : (el as any).value);
-      const setElValue = (value: any) => {
-        if (isCheckbox) {
-          el.checked = value;
-        } else {
-          (el as any).value = value;
+      const getElValue = () => {
+        if (isCheckboxOrRadio) {
+          return el.type === 'checkbox' ? el.checked : el.value;
         }
+
+        if (el instanceof HTMLSelectElement && el.multiple) {
+          return [].slice
+            .call(el.selectedOptions)
+            .map((option: HTMLOptionElement) => option.value || option.text);
+        }
+
+        return (el as any).value;
+      };
+      const setElValue = (value: any) => {
+        if (isCheckboxOrRadio) {
+          el.checked = value;
+          return;
+        }
+
+        if (el instanceof HTMLSelectElement && el.multiple) {
+          el.selectedIndex = 0;
+
+          [].slice.call(el.options).forEach((option: HTMLOptionElement) => {
+            if (value.indexOf(option.value || option.text) > -1) {
+              option.selected = true;
+            } else {
+              option.selected = false;
+            }
+          });
+
+          return;
+        }
+
+        (el as any).value = value;
       };
 
-      if (!isCheckbox && getElValue() !== '') {
+      if (!isCheckboxOrRadio && getElValue() !== '') {
         setModelValue(getElValue());
       } else {
         setElValue(getModelValue());
       }
 
-      el.addEventListener(eventName, () => setModelValue(getElValue()));
+      el.addEventListener(eventName, (event) => {
+        if (event instanceof CustomEvent && event.detail !== undefined) {
+          setModelValue(event.detail ?? (event.target as any).value);
+        } else {
+          setModelValue(getElValue());
+        }
+      });
 
       context.$watch(exp, setElValue);
     },
