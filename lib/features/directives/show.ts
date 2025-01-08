225d262edf04ade77e272e1f3da944fe95ab7b94
Hammed Oyedele <itz.harmid@gmail.com>
2025-01-08 09:30:38 +0100

    refactor: update show.ts

--- lib/features/directives/show.ts
+++ lib/features/directives/show.ts
@@ -4,7 +4,9 @@ export const showDirective: WickedStateDirectiveContract = {
   name: 'show',
   priority: 2,
   handler({ value, node }) {
-    if ( ! value) {
+    const evaluatedValue = evaluator(value, state);
+
+    if ( ! evaluatedValue) {
       node.style.display = 'none';
 
       return () => node.style.display = '';
