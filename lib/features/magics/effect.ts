246d4cd88910294a6cfec36e0fd1acc837432e22
Hammed Oyedele <itz.harmid@gmail.com>
2025-01-10 01:33:44 +0100

    perf: unsubscribe effects when component is destroyed

--- lib/features/magics/effect.ts
+++ lib/features/magics/effect.ts
@@ -1,8 +1,20 @@
 import {reactivity} from '../reactivity';
-import { WickedStateMagicContextContract } from '../../utils/contracts';
+import {WickedStateMagicContextContract} from '../../utils/contracts';
 
-export function effectMagic({ state }: WickedStateMagicContextContract): (fn: () => void) => void {
+export function effectMagic({ root, state }: WickedStateMagicContextContract): (fn: () => void) => void {
   return function effectMagicHandler(fn: () => void): void {
-    reactivity.effect(fn.bind(state));
+    const effectKey = '_state_effects';
+
+    const unsub = reactivity.effect(fn.bind(state));
+
+    if ( ! root.__wickedStateCleanups) {
+      root.__wickedStateCleanups = {};
+    }
+
+    if ( ! root.__wickedStateCleanups[effectKey]) {
+      root.__wickedStateCleanups[effectKey] = [];
+    }
+
+    root.__wickedStateCleanups[effectKey].push(unsub);
   };
 }
