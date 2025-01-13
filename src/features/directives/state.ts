7ce4119d8f891b4268bfc86995e3daf3bcad9913
Hammed Oyedele <itz.harmid@gmail.com>
2025-01-13 17:49:17 +0100

    feat: add proper ignore directive

--- src/features/directives/state.ts
+++ src/features/directives/state.ts
@@ -9,6 +9,10 @@ export const stateDirective: WickedStateDirectiveContract = {
     name: 'state',
     priority: 0,
     handler({node, value}): void {
+        if (node.__wickedStateProcessed) {
+            return;
+        }
+
         const expression = value === '' ? '{}' : value;
 
         const magicContext = decorateWithMagics({}, (node.__wickedStateCurrentElement = node));
